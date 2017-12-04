9d9e9c7e88343de2623b0b6e7fb6f5c6c5d8d1a7
Lucas de Faria Polaquini <cmte.lucas@hotmail.com>
2017-12-04 11:16:15 -0200

    XF webview

---
+++ dMagnets/dMagnets/dMagnets/View/WebView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using AndroidSpecific = Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+namespace dMagnets.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WebView : ContentPage
+    {
+        public WebView()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            AndroidSpecific.Application.SetWindowSoftInputModeAdjust(this, AndroidSpecific.WindowSoftInputModeAdjust.Resize);
+        }
+    }
+}
